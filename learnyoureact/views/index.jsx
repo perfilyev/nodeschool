b243be9cbfeab1c35d3c47c3a3969209fe433de4
Igor Perfilyev <perfilyevhead@gmail.com>
2016-06-23 07:28:20 +0000

    step 2: react components

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
 export default function TodoBox() {
-  return <div className="todoBox">Hello, world!</div>;
+  return (
+    <div className="todoBox">
+      <h1>Todos</h1>
+      <TodoList />
+      <TodoForm />
+    </div>);
+}
+
+
+function TodoList() {
+  return (<div className="todoList">I am a TodoList.</div>);
+}
+
+function TodoForm() {
+  return (<div className="todoForm">I am a TodoForm.</div>);
 }
